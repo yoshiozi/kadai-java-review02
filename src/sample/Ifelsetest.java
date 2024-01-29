b54db47c2843c7c76d180152fce17a439e46750e
yoshiozi <ka_i.y@me.com>
2024-01-29 20:12:27 +0900

    メッセージ追加

---
+++ src/sample/Ifelsetest.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Ifelsetest {
+
+    public static void main(String[] args) {
+        int score = 60; // 成績
+
+        String result = (score >= 60) ? "合格です！" : "赤点です...";
+            System.out.println(result);
+        }
+    }
+
+//変更
